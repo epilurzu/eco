b959134dc8b09792bf882cb27c03d5e1e5c00f07
Enrico <e.ipodda@gmail.com>
2020-06-12 14:58:57 +0200

    [feat] add topojson  renderer

--- src/map/Geochart.js
+++ src/map/Geochart.js
@@ -1,83 +1,65 @@
 import React, { useRef, useEffect, useState } from "react";
-import { select, geoPath, geoMercator, min, max, scaleLinear } from "d3";
+import { select, geoPath, geoMercator } from "d3";
+import * as topojson from "topojson-client";
 import useResizeObserver from "./useResizeObserver";
 
-import europe_data from "./europe.geo.json";
 /**
- * Component that renders a map of Germany.
+ * Component that renders a map with 3 layers:
+ *    europe;
+ *    natura 2000 areas;
+ *    corridor given as input.
  */
 
-function GeoChart() {
+function GeoChart({ europe, natura2000, corridor }) {
   const svgRef = useRef();
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
   const [selectedCountry, setSelectedCountry] = useState(null);
-  
-  const [flag, setFlag] = useState(false);
-  
-  const europe = europe_data;
-  const property = "iso_a2";
+
+  const states = topojson.feature(europe, europe.objects.europe);
+  const areas = topojson.feature(natura2000, natura2000.objects.IT);
+  const patches = topojson.feature(corridor, corridor.objects.Corr_Dec_1);
 
   // will be called initially and on every data change
   useEffect(() => {
     const svg = select(svgRef.current);
 
-    // use resized dimensions
-    // but fall back to getBoundingClientRect, if no dimensions yet.
+    // use resized dimensions, but fall back to getBoundingClientRect, if no dimensions yet.
     const { width, height } =
       dimensions || wrapperRef.current.getBoundingClientRect();
-    
+
     // projects geo-coordinates on a 2D plane
     const projection = geoMercator()
-    //.fitExtent([[-1.3*width, -1.7*height], [2.3*width, 1.3*height]], selectedCountry || europe)
-    .fitSize([width, height], selectedCountry || europe)
-    .precision(100);
+      .fitSize([width, height], patches)
+      .precision(100);
 
-    //.fitExtent([[-width, -height/10*9], [width/4*8, height]], selectedCountry || europe)
-    //.precision(100);
-
-
-    console.log(width +" "+ height)
-
-    // takes geojson data,
-    // transforms that into the d attribute of a path element
+    // transforms json data into the d attribute of a path element
     const pathGenerator = geoPath().projection(projection);
 
-    // render each state
+    // render states
     svg
       .selectAll(".state")
-      .data(europe.features)
+      .data(states.features)
       .join("path")
       .attr("class", "state")
-      .attr("d", feature => pathGenerator(feature));
-    
-    // render areas
-    //if (flag) {
-    //  svg
-    //    .selectAll(".area")
-    //    .data(it.features)
-    //    .join("path")
-    //    .attr("class", "area")
-    //    .attr("d", feature => pathGenerator(feature))
-    //    ;
-    //}
+      .attr("d", (feature) => pathGenerator(feature));
 
-    // render text
+    // render areas
     svg
-      .selectAll(".label")
-      .data([selectedCountry])
-      .join("text")
-      .attr("class", "label")
-      .text(
-        feature =>
-          feature &&
-          feature.properties.name +
-            ": " +
-            feature.properties[property].toLocaleString()
-      )
-      .attr("x", 10)
-      .attr("y", 50);
-  }, [europe, dimensions, property, selectedCountry]);
+      .selectAll(".area")
+      .data(areas.features)
+      .join("path")
+      .attr("class", "area")
+      .attr("d", (feature) => pathGenerator(feature));
+
+    // render patches
+    svg
+      .selectAll(".patch")
+      .data(patches.features)
+      .join("path")
+      .attr("class", "patch")
+      .attr("d", (feature) => pathGenerator(feature));
+  }, [states, areas, corridor, dimensions]);
 
   return (
     <div className={"map-container"} ref={wrapperRef}>
