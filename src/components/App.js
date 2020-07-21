46ec072ba4047ca505f4c4ec1be05ada075d366d
Enrico <e.ipodda@gmail.com>
2020-07-21 19:27:53 +0200

    [feat] compute components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "../assets/css/App.css";
 
+// Core
+import { compute_vcn } from "./core/utils";
+
 // Components
 import ProminentAppBar from "./prominent_app_bar/ProminentAppBar";
 import Alert from "./alert/Alert";
@@ -14,13 +17,14 @@ import corridor_data from "./data/corridor.json";
 function App() {
   const [europe, setEurope] = useState(europe_data);
   const [natura2000, setNatura2000] = useState(natura2000_data);
-  const [corridor, setCorridor] = useState(null);
-  //const [corridor, setCorridor] = useState(corridor_data);
-  console.log(corridor);
+  //const [corridor, setCorridor] = useState(null);
+  const [corridor, setCorridor] = useState(corridor_data);
+  compute_vcn(corridor);
+  //console.log(corridor);
+  //<GeoChart europe={europe} natura2000={natura2000} corridor={corridor} />
   return (
     <div className="App">
       <ProminentAppBar />
-      <GeoChart europe={europe} natura2000={natura2000} corridor={corridor} />
       <Alert corridor={corridor} onUploadCorridor={setCorridor} />
     </div>
   );
