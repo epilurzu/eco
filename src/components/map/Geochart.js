93befaaed048d812df3c6cc02499ab63da5e03c4
Enrico <e.ipodda@gmail.com>
2021-06-09 20:11:39 +0200

    [feat] convert to OOP

--- src/components/map/Geochart.js
+++ src/components/map/Geochart.js
@@ -4,7 +4,6 @@ import * as topojson from "topojson-client";
 import useResizeObserver from "./useResizeObserver";
 import SimpleCard from "./SimpleCard";
 import SimpleMenu from "./SimpleMenu";
-import { LocalConvenienceStoreOutlined } from "@material-ui/icons";
 
 /**
  * Component that renders a map with 3 layers:
@@ -50,46 +49,81 @@ function show(className, isShowed) {
   });
 }
 
-function GeoChart({ europe, natura2000, corridor, selectedId, updateAppSetState }) {
-  const svgRef = useRef();
-  const wrapperRef = useRef();
-  const dimensions = useResizeObserver(wrapperRef);
-  const [selectedCountry, setSelectedCountry] = useState(null);
+class GeoChart extends React.Component {
 
-  const states = topojson.feature(europe, europe.objects[Object.keys(europe.objects)[0]]);
-  const areas = topojson.feature(natura2000, natura2000.objects[Object.keys(natura2000.objects)[0]]);
-  const patches = corridor !== null ? topojson.feature(corridor, corridor.objects[Object.keys(corridor.objects)[0]]) : null;
-  var selectedPatches = null;
-  if (selectedId.length != 0) {
-    selectedPatches = {
-      "type": "FeatureCollection",
-      "features": patches.features.filter((feature) => selectedId.includes(feature.properties.OBJECTID))
+  constructor(props) {
+    super(props);
+
+    this.svgRef = React.createRef();
+    this.wrapperRef = React.createRef();
+
+    this.states = topojson.feature(props.europe, props.europe.objects[Object.keys(props.europe.objects)[0]]);
+    this.areas = topojson.feature(props.natura2000, props.natura2000.objects[Object.keys(props.natura2000.objects)[0]]);
+
+    this.updateAppSetState = props.updateAppSetState;
+
+    this.state = {
+      selectedId: props.selectedId,
+      patches: props.corridor !== null ? topojson.feature(props.corridor, props.corridor.objects[Object.keys(props.corridor.objects)[0]]) : null,
+      selectedPatches: null,
+      selectedCountry: null,
     };
+
+    this.isIdSelected = (feature) => {
+      if (this.state.selectedId.includes(feature.properties.OBJECTID)) {
+        return "patch selected";
+      }
+      return "patch";
+    };
+
   }
 
-  function isIdSelected(feature, selectedId) {
-    if (selectedId.includes(feature.properties.OBJECTID)) {
-      return "patch selected";
+  componentDidMount() {
+    if (this.state.selectedId.length != 0) {
+      this.setState({
+        selectedPatches: {
+          "type": "FeatureCollection",
+          "features": patches.features.filter((feature) => selectedId.includes(feature.properties.OBJECTID))
+        }
+      });
     }
-    return "patch";
-  }
 
-  // will be called initially and on every data change
-  useEffect(() => {
+
+
+
+
+
+    const dimensions = new ResizeObserver((entries) => {
+      //do things
+    });
+
+    //console.log(dimensions);
+    //console.log(this.wrapperRef);
+    dimensions.observe(document.getElementById("pippo"));
+
+
+
+
+
+
+
+
+
     // use resized dimensions, but fall back to getBoundingClientRect, if no dimensions yet.
     const { width, height } =
-      dimensions || wrapperRef.current.getBoundingClientRect();
+      //dimensions || 
+      this.wrapperRef.current.getBoundingClientRect();
 
     // projects geo-coordinates on a 2D plane
     const projection = d3.geoMercator()
-      .fitSize([width, height], selectedPatches || patches || areas)
+      .fitSize([width, height], this.state.selectedPatches || this.state.patches || this.areas)
       .precision(100);
 
     // remove all existing svg
-    d3.select(svgRef.current).selectAll("svg").remove();
+    //d3.select(this.svgRef.current).selectAll("svg").remove();
 
     // create svg
-    const svg = d3.select(svgRef.current)
+    const svg = d3.select(this.svgRef.current)
       .call(d3.zoom()
         //.scaleExtent([1 / 50, Infinity])
         .on("zoom", function (event) {
@@ -104,49 +138,49 @@ function GeoChart({ europe, natura2000, corridor, selectedId, updateAppSetState 
     // render states
     svg
       .selectAll(".state")
-      .data(states.features)
+      .data(this.states.features)
       .join("path")
       .attr("class", "state")
-      .attr("d", (feature) => pathGenerator(feature));
+      .attr("d", (feature) => d3.geoPath().projection(projection)(feature));
 
     // render areas
     svg
       .selectAll(".area")
-      .data(areas.features)
+      .data(this.areas.features)
       .join("path")
       .attr("class", "area")
       .attr("d", (feature) => pathGenerator(feature));
 
     //render patches
-    if (patches != null) {
+    if (this.state.patches != null) {
       svg
         .selectAll(".patch")
-        .data(patches.features)
+        .data(this.state.patches.features)
         .join("path")
         .attr("data-id", (feature) => feature.properties.OBJECTID)
         .attr("data-centroid", (feature) => getBoundingBoxCenter(feature.geometry))
-        .attr("class", (feature) => isIdSelected(feature, selectedId))
+        .attr("class", (feature) => this.isIdSelected(feature, this.state.selectedId))
         .attr("d", (feature) => pathGenerator(feature))
-        .on("click", function (event) {
+        .on("click", (event) => {
           let id = parseInt(event.target.dataset.id);
 
-          let tempSelectedId = selectedId.slice();
-          if (!selectedId.includes(id)) {
+          let tempSelectedId = this.state.selectedId.slice();
+          if (!this.state.selectedId.includes(id)) {
             tempSelectedId.push(id);
-            updateAppSetState({ selectedId: tempSelectedId });
+            this.updateAppSetState({ selectedId: tempSelectedId });
             event.target.classList.add("selected");
           }
           else {
-            let index = selectedId.indexOf(id);
+            let index = this.state.selectedId.indexOf(id);
             tempSelectedId.splice(index, 1);
-            updateAppSetState({ selectedId: tempSelectedId });
+            this.updateAppSetState({ selectedId: tempSelectedId });
             event.target.classList.remove("selected");
           }
         });
     }
 
     svg.selectAll(".patch")
-      .each(function (feature, i) {
+      .each((feature, i) => {
         svg.append('circle')
           .attr("class", "circle network")
           .attr('cx', projection(getBoundingBoxCenter(feature.geometry))[0])
@@ -156,15 +190,12 @@ function GeoChart({ europe, natura2000, corridor, selectedId, updateAppSetState 
       });
 
     svg.selectAll(".patch")
-      .each(function (feature, i) {
+      .each((feature, i) => {
         feature.properties.NEIGHBORS.split(",").forEach((neighbor, index) => {
-          let n_patch = patches.features.find((feature) => feature.properties.OBJECTID == neighbor)
+          let n_patch = this.state.patches.features.find((feature) => feature.properties.OBJECTID == neighbor)
           if (n_patch !== undefined && feature !== undefined) {
             if (n_patch.geometry !== null && feature.geometry !== null) {
 
-              if (parseInt(projection(getBoundingBoxCenter(patches.features.find((feature) => feature.properties.OBJECTID == neighbor).geometry))[0]) < 0) {
-                console.log(patches.features.find((feature) => feature.properties.OBJECTID == neighbor));
-              }
               let x1 = projection(getBoundingBoxCenter(feature.geometry))[0];
               let y1 = projection(getBoundingBoxCenter(feature.geometry))[1];
               let x2 = projection(getBoundingBoxCenter(n_patch.geometry))[0];
@@ -183,15 +214,20 @@ function GeoChart({ europe, natura2000, corridor, selectedId, updateAppSetState 
         })
       });
 
-  }, [states, areas, corridor, dimensions, selectedId, isIdSelected, updateAppSetState]);
+  }
+  componentDidUpdate() {
+    //document.title = `You clicked ${this.state.count} times`;
+  }
 
-  return (
-    <div className={"map-container"} ref={wrapperRef}>
-      <SimpleMenu show={show} />
-      <SimpleCard patches={patches} selectedId={selectedId.slice()} updateAppSetState={updateAppSetState} />
-      <svg className={"map"} ref={svgRef}></svg>
-    </div>
-  );
+  render() {
+    return (
+      <div className={"map-container"} id={"pippo"} ref={this.wrapperRef}>
+        <SimpleMenu show={show} />
+        <SimpleCard patches={this.state.patches} selectedId={this.state.selectedId.slice()} updateAppSetState={this.updateAppSetState} />
+        <svg className={"map"} ref={this.svgRef}></svg>
+      </div>
+    );
+  }
 }
 
 export default GeoChart;
